8893c1fbaf7ace0f18a7f4c019e1693890fb2d0a
emmamclarnon <emclarnon@hotmail.com>
2026-08-09 19:58:41 +0100

    yaml and maths tutor

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -2,7 +2,10 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-3.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name='math_tutor_agent', #More specific internal name
+    description='Helps students learn algebra by guiding them through problem-solving steps',
+    instruction='You are a patient math tutor. Help students with algebra problems and be empathetic when returning responses',
 )
+
+
+
